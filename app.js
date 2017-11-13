4afbb28aef07fcb34f41b65510a48a88f6c43b6c
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2017-11-13 14:57:01 +0100

    no message

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Requires
 const http = require('http');
 const httpProxy = require('http-proxy');
-const proxy = httpProxy();
 
 // Create a new reverse proxy
 const proxy = httpProxy.createProxyServer();
