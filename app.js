7463d1d9eaa50a21f76214632161c5edbd66a21e
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2017-11-13 14:46:11 +0100

    no message

---
+++ app.js
@@ -0,0 +1,13 @@
+// Requires
+const http = require('http');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy();
+
+// Create a new reverse proxy
+const proxy = httpProxy();
+
+// Create a new webserver
+http.createServer((req,res)=> {
+	// Can we read the incoming url?
+	res.write(res.url);
+}).listen(80);
